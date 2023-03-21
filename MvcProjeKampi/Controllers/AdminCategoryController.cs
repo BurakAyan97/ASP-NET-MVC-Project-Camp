0230cc352c06344cffc6a47801a59bad623aff5b
BurakAyan97 <burakayan97@outlook.com>
2023-03-21 22:01:38 +0300

    updated

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCatagoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator validationRules = new CategoryValidator();
+            ValidationResult results = validationRules.Validate(p);
+            if (results != null)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryValue = cm.GetByID(id);
+            cm.CategoryDelete(categoryValue);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);
+            return View(categoryvalue);
+        }
+
+        [HttpPost]
+        public ActionResult EditCategory(Category p)
+        {
+            cm.CategoryUpdate(p);
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
